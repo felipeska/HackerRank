b181cb7c8b3f1c0e32bbb405e4d55bdb6909ec65
felipeska <felipeskabarragan@gmail.com>
2020-07-17 14:43:58 -0500

    Permutation equation

--- src/main/kotlin/com/felipecb/PermutationEquation.kt
+++ src/main/kotlin/com/felipecb/PermutationEquation.kt
@@ -1,21 +1,26 @@
 package com.felipecb
 
-import java.util.*
+import java.util.Scanner
+
+// https://www.hackerrank.com/challenges/permutation-equation/problem
 
 // Complete the permutationEquation function below.
 fun permutationEquation(p: Array<Int>): Array<Int> {
-
-    return arrayOf(0)
+  val y = IntArray(p.size)
+  for (i in p) {
+    y[p[p[i - 1] - 1] - 1] = i
+  }
+  return y.toTypedArray()
 }
 
 fun main(args: Array<String>) {
-    val scan = Scanner(System.`in`)
+  val scan = Scanner(System.`in`)
 
-    val n = scan.nextLine().trim().toInt()
+  val n = scan.nextLine().trim().toInt()
 
-    val p = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+  val p = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
 
-    val result = permutationEquation(p)
+  val result = permutationEquation(p)
 
-    println(result.joinToString("\n"))
+  println(result.joinToString("\n"))
 }
